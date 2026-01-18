5ab341e61b83ad6b67e0d595b98ba213b22a8994
adrianistaken <adrian.rodriguez.dev@gmail.com>
2026-01-18 09:42:10 -0600

    Add Sentry integration and update dependencies
    - Integrated Sentry for error tracking and performance monitoring in main.ts.
    - Updated package.json and package-lock.json to include new dependencies: @sentry/vue and @types/node.
    - Updated various dependencies to their latest versions.
    - Added .env to .gitignore to prevent sensitive information from being tracked.
    - Adjusted tsconfig.json to include .d.ts files in the compilation process.

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,25 @@ import VueKonva from 'vue-konva'
 import './style.css'
 import App from './App.vue'
 import { inject } from '@vercel/analytics';
+import * as Sentry from '@sentry/vue';
 
 const app = createApp(App)
 const pinia = createPinia()
+
+Sentry.init({
+    app,
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    sendDefaultPii: true,
+    integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.replayIntegration()
+    ],
+    tracesSampleRate: 1.0,
+    tracePropagationTargets: ["localhost"],
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+    debug: false,
+});
 
 app.use(pinia)
 app.use(VueKonva)
